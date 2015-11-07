6c349de0db0e85afc7bf5534be121f7017f34364
Seiwert Quentin <quentin.seiwert@telecomnancy.net>
2015-11-07 21:00:02 +0100

    World projectile

--- src/fr/world/World.java
+++ src/fr/world/World.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import fr.entity.character.Player;
+import fr.entity.projectile.Projectile;
 
 
 public class World extends BasicGameState{
@@ -14,20 +15,24 @@ public class World extends BasicGameState{
 	public enum direction {HAUT,DROITE,BAS,GAUCHE};
 	private static Player player;
 	public static int ID=0;
+	private static Projectile p1;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		player = new Player();
+		p1 = new Projectile();
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
 		player.render(container, game, g);
+		p1.render(container,game,g);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		player.update(container, game, delta);
+		p1.update(container, game,delta);
 	}
 	
 	public void keyReleased(int key, char c) {
