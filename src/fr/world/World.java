83e04f8825374206705adf6fdafba3ee1d6d2eb6
Seiwert Quentin <quentin.seiwert@telecomnancy.net>
2015-11-07 20:24:48 +0100

    World player

--- src/fr/world/World.java
+++ src/fr/world/World.java
@@ -6,33 +6,36 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import fr.entity.character.Player;
+
 
 public class World extends BasicGameState{
 	
 	public enum direction {HAUT,DROITE,BAS,GAUCHE};
+	private static Player player;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-
+		player = new Player();
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-
+		player.render(container, game, g);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-
+		player.update(container, game, delta);
 	}
 	
 	public void keyReleased(int key, char c) {
-
+		player.keyReleased(key, c);
 	}
 
 
 	public void keyPressed(int key, char c) {
-
+		player.keyReleased(key, c);
 	}
 
 	@Override
