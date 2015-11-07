0c4a508734c1f1117ff51ac5bc4901a6fd33ba7e
Seiwert Quentin <quentin.seiwert@telecomnancy.net>
2015-11-07 23:20:19 +0100

    recup

--- src/fr/entity/character/enemy/BasicEnemy.java
+++ src/fr/entity/character/enemy/BasicEnemy.java
@@ -6,27 +6,39 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import fr.entity.projectile.type.ProjectileType0;
+
 
 public class BasicEnemy extends Enemy{
 	
 	float rotation;
+	int timeShoot;
+	int timeLeft;
 
 	public BasicEnemy(double x, double y, double width, double height) {
 		super(x, y, width, height);
 		speedX = 0.3;
+		timeShoot = 1;
+		timeLeft = timeShoot;
 	}
 	
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		super.update(container, game, delta);
 		rotation++;
-		y += Math.sin(x/30)*10;
+		if(timeLeft <= 0){
+			shoot(rotation+45);
+			shoot(rotation+135);
+			shoot(rotation+225);
+			shoot(rotation+315);
+			timeLeft = timeShoot;
+		}else{
+			timeLeft--;
+		}
 	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
 		g.setColor(Color.red);
-		g.rotate((float)x, (float)y, rotation);
 		g.fillRect((float)x,(float)y,(float)width,(float)height);
-		
 	}
 	
 	public void move(int delta){
@@ -34,12 +46,10 @@ public class BasicEnemy extends Enemy{
 		if(x<0){
 			x = 0;
 			speedX = -speedX;
-			speedY = -0.05;
 		}
 		if(x>800-width){
 			x = 800 - width;
 			speedX = -speedX;
-			speedY = 0.05;
 		}
 		moveY(delta);
 		if(y<0){
@@ -50,6 +60,10 @@ public class BasicEnemy extends Enemy{
 			y = 600-height;
 			speedY = -speedY;
 		}
+	}
+	
+	void shoot(float rot){
+		new ProjectileType0((double) x+(width/2)-8,(double)y+(height/2)-8,rot);
 	}
 
 
