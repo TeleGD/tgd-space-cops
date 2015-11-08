187e71055113e972c6a9da9307ee557a56d2bf9d
Arthur <canalarthur@yahoo.fr>
2015-11-08 04:17:16 +0100

    Projectiles sinusoidaux

--- src/fr/entity/character/enemy/EnemyGenerator.java
+++ src/fr/entity/character/enemy/EnemyGenerator.java
@@ -6,7 +6,8 @@ import org.newdawn.slick.state.StateBasedGame;
 public class EnemyGenerator {
 	
 	public void init(GameContainer container,StateBasedGame game){
-		new BasicEnemy(400-16,300-16,32,32,1000);
+		//new BasicEnemy(400-16,300-16,32,32,1000);
+		new CircleEnemy(400-16,300-16,32,32,1000,3);
 	}
 
 }
