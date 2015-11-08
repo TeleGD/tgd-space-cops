46739abd070e7991f580bf5448cb78df3caf20ff
Seiwert Quentin <quentin.seiwert@telecomnancy.net>
2015-11-08 02:26:19 +0100

    recup

--- src/fr/entity/character/enemy/EnemyGenerator.java
+++ src/fr/entity/character/enemy/EnemyGenerator.java
@@ -6,7 +6,11 @@ import org.newdawn.slick.state.StateBasedGame;
 public class EnemyGenerator {
 	
 	public void init(GameContainer container,StateBasedGame game){
-		new CircleEnemy(400,200,32,32,1000,10);
+		new CircleEnemy(600,200,32,32,1000,12);
+		new CircleEnemy(200,200,32,32,1000,12);
+		new BasicEnemy(200,200,32,32,100);
+		new BasicEnemy(200,200,32,32,100);
+		new BasicEnemy(200,200,32,32,100);
 	}
 
 }
